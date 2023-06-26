ad66d54c6a7d794cdf2c846ff83c6cf4b6a1181b
Tim Xylon <zedifen@outlook.com>
2023-06-26 18:50:20 +0000

    Bump version to v1.3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.v2ray"
         minSdkVersion(23)
         targetSdkVersion(33)
-        versionCode = 1030700
-        versionName = "1.3.7"
+        versionCode = 1030800
+        versionName = "1.3.8"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
