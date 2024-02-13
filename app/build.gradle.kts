27c1347290e37badfa7a1e6c7b9509438f4896d4
Enif Zed <108571201+zedifen@users.noreply.github.com>
2024-02-13 16:12:24 +0800

    Bump version to v1.3.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.v2ray"
         minSdkVersion(23)
         targetSdkVersion(33)
-        versionCode = 1031300
-        versionName = "1.3.13"
+        versionCode = 1031400
+        versionName = "1.3.14"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
