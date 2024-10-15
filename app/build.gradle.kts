e920317ff794e2080380052ac1c52bd07762e445
Enif Zed <108571201+zedifen@users.noreply.github.com>
2024-10-15 23:38:56 +0000

    Filter out unwanted (unsupported) abi targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,9 @@ android {
     }
     splits {
         abi {
+            reset()
             isEnable = true
+            include ("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
             isUniversalApk = true
         }
     }
